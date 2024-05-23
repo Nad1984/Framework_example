32589538469f646ceed40e86efa8a60b3f245341
Nad1984 <71829239+Nad1984@users.noreply.github.com>
2024-05-23 21:59:27 +0300

    Delete src directory

--- src/pages/locators/MyAccountSignedInLocators.py
+++
@@ -1,5 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class MyAccountSignedInLocators:
-    LEFT_NAV_LOGOUT_BUTTON = (By.CSS_SELECTOR, 'li.woocommerce-MyAccount-navigation-link--customer-logout')
--- src/pages/locators/OrderReceivedPageLocators.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class OrderReceivedPageLocators:
-    ORDER_RECEIVED_TITLE = (By.CSS_SELECTOR, 'header h1.entry-title')
-    ORDER_NUMBER = (By.CSS_SELECTOR, 'li.order strong')
